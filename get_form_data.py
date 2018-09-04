3658f30679413e8ab188a0301fc9608a4d0051a4
Badbeastt <aneeshshukla96@gmail.com>
2018-09-04 15:21:50 +0530

    initial commit

---
+++ get_form_data.py
@@ -0,0 +1,35 @@
+import cgi
+
+def htmlTop():
+    print("""Content-type:text/html\n\n
+        <!DOCTYPE html>
+        <html lang="en">
+            <head>
+                <meta charset="utf-8"/>
+                <title>Answers</title>
+            </head>
+            <body>""")
+
+def htmlTail():
+    print("""</body>
+        </html>""")
+
+def getData(num):
+    formData = cgi.FieldStorage()
+    ans = formData.getvalue('ans_'+str(num));
+    return ans
+
+#main program
+if __name__ == "__main__":
+    try:
+        htmlTop()
+        ans_list = []
+        for x in range(253, 318):
+            ans_list.append(getData(x))
+        print("The answers are:\n{0}".format(ans_list))
+        htmlTail()
+    except:
+        cgi.print_exception()
+    
+    for x in range(0,len(ans_list))
+        browser.find_elements_by_css("input[type='radio'][value='%s']".format(ans_list[x])).click
